6f6d92e629833c798977e5f1fb42f453885daa2f
baothaihcmut <banbao22042004@gmail.com>
2024-10-12 10:10:04 +0700

    init

---
+++ api/printer-api/src/main/java/com/example/printer_api/PrinterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.printer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterApiApplication.class, args);
+	}
+
+}
---
+++ api/printer-api/src/test/java/com/example/printer_api/PrinterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.printer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
