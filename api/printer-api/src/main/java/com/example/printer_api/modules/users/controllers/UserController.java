6abe714fb69e592d9079c68a7a2d75021908779e
baothaihcmut <banbao22042004@gmail.com>
2024-10-17 16:53:11 +0700

    add handler

--- api/printer-api/src/main/java/com/example/printer_api/modules/users/controllers/UserController.java
+++ api/printer-api/src/main/java/com/example/printer_api/modules/users/controllers/UserController.java
@@ -2,12 +2,17 @@ package com.example.printer_api.modules.users.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.printer_api.modules.users.dtos.CreateUserDTO;
+import com.example.printer_api.shared.exception.StatusCode;
 import com.example.printer_api.shared.response.CustomReponse;
 import com.example.printer_api.shared.response.ResponseFactory;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -15,8 +20,9 @@ public class UserController {
     @Autowired
     private ResponseFactory responseFactory;
 
-    @GetMapping("/")
-    public ResponseEntity<CustomReponse> getHello() {
-        return ResponseEntity.ok().body(this.responseFactory.initResponse(true, "hello", null));
+    @PostMapping("")
+    public ResponseEntity<CustomReponse> createUser(@RequestBody @Valid CreateUserDTO userDTO) {
+        System.err.println("create User....");
+        return ResponseEntity.status(StatusCode.CREATED.getCode()).body(this.responseFactory.initResponse(true, "create user success", userDTO));
     }
 }
--- api/printer-api/src/main/java/com/example/printer_api/shared/handler/HttpExceptionHandler.java
+++ api/printer-api/src/main/java/com/example/printer_api/shared/handler/HttpExceptionHandler.java
@@ -1,16 +1,47 @@
-// package com.example.printer_api.shared.handler;
+package com.example.printer_api.shared.handler;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
 
-// import com.example.printer_api.shared.entity.CustomReponse;
-// import com.example.printer_api.shared.exception.HttpException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// @RestControllerAdvice
-// public class HttpExceptionHandler {
-//     @ExceptionHandler(HttpException.class)
-//     public ResponseEntity<CustomReponse<Object>> handleHttpException(HttpException e) {
-//         return ResponseEntity.status(e.getStatusCode().getCode()).body()
-//     }
-// }
+import com.example.printer_api.shared.exception.HttpException;
+import com.example.printer_api.shared.response.CustomReponse;
+import com.example.printer_api.shared.response.ResponseFactory;
+import org.springframework.http.converter.HttpMessageConversionException;
+
+@RestControllerAdvice
+public class HttpExceptionHandler {
+    @Autowired
+    private ResponseFactory  responseFactory;
+
+    @ExceptionHandler(HttpException.class)
+    public ResponseEntity<CustomReponse> handleHttpException(HttpException e) {
+        return ResponseEntity.status(e.getStatusCode().getCode()).body(this.responseFactory.initResponse(false, e.getMessage(), null));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<CustomReponse> handleHttpException(RuntimeException e) {
+        System.err.println(e.getClass());
+        return ResponseEntity.status(500).body(this.responseFactory.initResponse(false, "Internal error", null));
+    }
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<CustomReponse> handleBadRequest(HttpMessageConversionException e) {
+        return ResponseEntity.status(400).body(this.responseFactory.initResponse(false,"Wrong Data type",null));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomReponse> handleDTOException(MethodArgumentNotValidException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.status(400).body(this.responseFactory.initResponse(false,"Unvalid Body",errorMap));
+    }
+
+    
+}
